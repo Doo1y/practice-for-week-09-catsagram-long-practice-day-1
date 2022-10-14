e6ad6fe8401beab9f41ba7504ce7a004a17fc41d
Samuel Park <s.park111497@gmail.com>
2022-10-14 02:36:21 -0700

    finished assignment

--- public/index.js
+++ public/index.js
@@ -1,1 +1,20 @@
 // Your code here
+
+window.addEventListener("DOMContentLoaded", event => {
+  // instantiate a h1 heading element
+  const header = document.createElement("h1");
+  header.id = "kitten-header";
+  header.innerText = "Kitten Pic";
+
+  // fetch the link
+
+  // instantiate a kitten element 
+  const kittenImg = document.createElement("img");
+  kittenImg.alt = "a photo of a kitten";
+  kittenImg.src = "https://cdn2.thecatapi.com/images/7bp.jpg";
+  
+  // kittenImg.src = `${fetchKittenUrl().url}`;
+
+  document.body.prepend(header);
+  document.body.appendChild(kittenImg)
+});
